f8ddd7393e9c6c7267eb54b073689444d09dd63c
irinasams <63508578+irinasams@users.noreply.github.com>
2021-02-09 12:31:04 +0100

    initial commit

---
+++ js/config.js
@@ -0,0 +1,8 @@
+
+window._mtm = window._mtm || {};
+window._mtm.projectId = "prj:06f2dd83-744f-49e1-8c8f-5ec5ae8d0feb";
+window._mtm.configAutoblocking =
+{
+    "rules": [],
+    "autoblockingEnabled": true
+}
